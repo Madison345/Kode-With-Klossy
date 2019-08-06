c44dfa5baf0a674a735aaa9593f14a71f227e1fe
Apple <apple@Apples-MacBook-Pro-97.local>
2019-08-06 10:27:59 -0700

    new

---
+++ kwk day 2.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+var str = "Hello, playground"
+//DICTIONARIES
+//Similar to arrays but used to store associative information
+//each element in a dictionary has a key and a value
+////2 ways to create dictionaries: with data or empty
+////Creating an empty dictionary
+//var perfectTen : [String : String] =
+//    [:]
+//print(perfectTen)
+//
+////Creating a dictionary with data
+//var perfectEleven = ["Almond flour" : "3 and 1/2 cups", "gluten-free oats" : "2 cups", "dark chocolate chips" : "1 cup"]
+////Almond flour, gluten-free oats and dark chocolate chips are all keys in our dictionary
+////3 and 1/2 cups, 2 cups and 1 cup are all values in our dictionary
+//print(perfectEleven)
+//
+////Adding or change the value of a key:
+//perfectEleven["dark chocolate chips"] = "2 cups"
+//print(perfectEleven)
+//
+////Printing just values or just keys
+//print(perfectEleven.keys)
+//print(perfectEleven.values)
+//
+////printing just the value for one day
+//print(perfectEleven["gluten-free oats"])
+//
+////use an exclamation point to 'unwrap' the value assigned to the key we called, to assure swift that it has an associated value
+////adding keys and values to a dictionary
+//perfectEleven["vanilla"] = "1 tsp"
+//print(perfectEleven)
+//
+////remove keys and values in a dictionary
+//perfectEleven["gluten-free oats"] = nil
+//print(perfectEleven)
+//nil means nonexistent
+
+//var familyFriend : [String : String] =
+//[:]
+//print(familyFriend)
+//
+//var FamilyTree = ["Marquesha" : "Kory", "Leiah" : "Auntie Cha"]
+//print(FamilyTree)
+//
+//FamilyTree["Auntie Cha"] = "Marquesha"
+//print(FamilyTree)
+//
+//print(FamilyTree.keys)
+//print(FamilyTree.values)
+//
+//print(FamilyTree["Kory"])
+//
+//FamilyTree["Leiah"] = "Cousin"
+//
+//FamilyTree["Kory"] = nil
+//print(FamilyTree)
+
